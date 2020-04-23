66742c80fa665cdfe44389ae9c7f88312b337de2
David Díaz González <dgonzalez@suse.de>
2020-04-24 00:08:07 +0100

    WIP: Re-layout the Participant component

--- src/components/Participant/Participant.js
+++ src/components/Participant/Participant.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) [2015-2019] SUSE Linux
+ * Copyright (c) [2015-2020] SUSE Linux
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE.txt file for details.
@@ -14,8 +14,7 @@ import ToggleVideo from '../ToggleVideo';
 import StopScreenSharing from '../StopScreenSharing';
 import Reconnect from '../Reconnect';
 import { actionCreators as participantsActions } from '../../state/ducks/participants';
-
-import './Participant.css';
+import { classNames } from '../../utils/common';
 
 function setVideo(id, videoRef) {
   const stream = janusApi.getFeedStream(id);
@@ -30,27 +29,37 @@ function toggleFocus(id, focus) {
   return focus === 'user' ? participantsActions.unsetFocus() : participantsActions.setFocus(id);
 }
 
-function Participant({ id, display, isPublisher, isLocalScreen, streamReady, focus, video }) {
+function Participant({ id, display, isPublisher, isLocalScreen, streamReady, focus, speaking, audio, video }) {
   const dispatch = useDispatch();
   const videoRef = React.createRef();
-  const cssClassName = `Participant ${focus === 'user' ? 'focus' : ''}`;
 
   useEffect(() => setVideo(id, videoRef.current), [streamReady]);
 
   return (
-    <div className={cssClassName}>
+    <div className={classNames(
+      "relative group p-1 border-2 border-white bg-white",
+      "transition duration-150 ease-in-out",
+      focus === 'user' && "border-secondary shadow-md",
+      speaking && "border-blue-300"
+    )}>
       <video
         ref={videoRef}
         muted={isPublisher}
         autoPlay
-        className={isPublisher && !isLocalScreen ? 'mirrored' : ''}
+        className={classNames(
+          isPublisher && !isLocalScreen && 'mirrored'
+        )}
         onClick={() => dispatch(toggleFocus(id, focus))}
       />
-      <div className="display">{display}</div>
-      {!isLocalScreen && <MuteButton participantId={id} />}
-      {isPublisher && !isLocalScreen && <ToggleVideo video={video} />}
-      {isPublisher && isLocalScreen && <StopScreenSharing id={id} />}
-      {!isPublisher && <Reconnect participantId={id} />}
+      <div className="flex px-1 items-center bg-gray-200 text-center text-xs sm:text-sm md:text-base">
+        <span className="ml-1">{display}</span>
+      </div>
+      <div className="bg-primary-dark opacity-75 absolute inset-0 hidden group-hover:flex flex-wrap">
+        {isPublisher && isLocalScreen && <StopScreenSharing id={id} />}
+        {!isLocalScreen && <MuteButton participantId={id} />}
+        {isPublisher && !isLocalScreen && <ToggleVideo video={video} />}
+        {!isPublisher && <Reconnect participantId={id} />}
+      </div>
     </div>
   );
 }
